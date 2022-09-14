fa58a1317adde17182cf493e5f2bb836108abf33
Lemmynjash <lemmynjariakiarie@gmail.com>
2022-09-14 22:19:19 +0300

    not yet done

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Web3 = require("web3");
+import { abi } from "./contractApi";
 
 if (typeof web3 != "undefined") {
   web3 = new Web3(web3.currentProvider);
@@ -6,10 +7,12 @@ if (typeof web3 != "undefined") {
   web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:9545"));
 }
 
+var AppprovalContract = new web3.eth.Contract(abi, contractAddress);
+
 function checkIfTheyAreRealAccounts() {
   var fromAddress = "";
   var toAddress = "yyyyyyy";
-  var amount = "0";
+  var amount = "2";
 
   if (web3.utils.isAddress(toAddress) != true) {
     console.error(
@@ -21,6 +24,23 @@ function checkIfTheyAreRealAccounts() {
     console.error("You must send more than 0 ETH.");
     return;
   }
+
+  ApprovalContract.methods
+    .deposit(toAddress)
+    .send(
+      {
+        from: fromAddress,
+        gas: 100000,
+        value: web3.utils.toWei(amount, "ether"),
+      },
+      function (error, result) {
+        if (error) {
+          console.error("error: " + error);
+        } else {
+          console.log("Success: " + result);
+        }
+      }
+    );
 }
 
 checkIfTheyAreRealAccounts();
